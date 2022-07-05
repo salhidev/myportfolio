59ebf2d7d0cbfc41ad3985ebc701a75c255d0216
Ayoub Salhi <salhi.dev@gmail.com>
2022-07-05 12:38:10 -0400

    Adding components

---
+++ src/Components/About.js
@@ -0,0 +1,40 @@
+
+import React from "react";
+import picture from "../profile.jpeg"
+export default function About() {
+    return (
+        <section id="about">
+          <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                Hi, I'm Ayoub.
+                <br className="hidden lg:inline-block" />I love to build amazing
+                apps.
+              </h1>
+              <p className="mb-8 leading-relaxed">
+              Looking to join a collaborative organization to achieve high-quality customer-focused results with cutting edge technologies in a timely manner with a strong desire for everyone to learn and grow as a team. I have over 10 years of experience in software development, and experience with running point for multiple development projects. I have been instrumental in team collaboration with project management and source control and a keen eye for developing technical documentation.
+              </p>
+              <div className="flex justify-center">
+                <a
+                  href="#contact"
+                  className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  Work With Me
+                </a>
+                <a
+                  href="#projects"
+                  className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                  See My Past Work
+                </a>
+              </div>
+            </div>
+            <div className="lg:max-w-lg ">
+              <img
+                className="object-cover object-center rounded rounded-full ring-2 ring-white"
+                alt="profilePic"
+                src={picture}
+              />
+            </div>
+          </div>
+        </section>
+      );
+}
---
+++ src/data.js
@@ -0,0 +1,65 @@
+import ris from "./images/RIS.jpeg"
+export const projects = [
+    {
+      title: "RIS",
+      subtitle: ".Net Core and React",
+      description:
+        "Worked on rewriting the Regulation Information System (RIS) from Lotus Notes + COBOL + DB2 to .NET Core + ReactJS + SQL Server. The app is used by Virginia agencies to file regulations and related items with the Virginia Registrar of Regulations for publication in the Virginia Register of Regulations. Roles included full-stack development as well as contributing to the legacy data migration from Lotus Notes to SQL Server.",
+      image: "images/RIS.jpeg",
+      link: "https://ris.dls.virginia.gov",
+    },
+    {
+      title: "LIS",
+      subtitle: ".Net Core and React",
+      description:
+        "Instrumental in multi-year system rewrite of the Legislative Information System from COBOL + DB2 to .NET Core + Azure + SQL Server. Specific roles included architectural development of micro-services API layer, unit tests, API regression tests and Azure devops CI/CD pipelines. Project used Scrum methodologies.",
+      image: "images/LIS.jpeg",
+      link: "https://qa.lis.virginia.gov",
+    },
+    {
+      title: "Library Reference Center",
+      subtitle: ".Net Core and React",
+      description:
+        "Full Stack developement of library portal and administration using React + .Net Core 3.1 + SQL Server",
+      image: "images/lrc.jpeg",
+      link: "https://lrc.dls.virginia.gov",
+    },
+    {
+      title: "DLAS Contracts",
+      subtitle: ".Net Core and React",
+      description:
+        "Full Stack developement of the DLAS Contracts administration app using React + .Net Core 3.1 + SQL Server",
+      image:  "images/Contracts.jpeg",
+      link: "https://apps.dlas.virginia.gov/Contracts",
+    },
+  ];
+
+  export const skills = [
+        ".NET Core +	ASP.NET +  C# + VB.NET + Entity Framework",
+         "SQL Server + SQL Reporting Services + Red Gate products (Compare and Source Control)", 
+        "Git, Azure DevOps/VSTS/TFS" ,
+        "CI/CD (Azure DevOps Pipelines and Releases)",
+        "Azure Portal (Azure SQL, SQL Managed Instance, App Services, App Gateways, Blob Storage, Key Vaults",
+        "JavaScript + AngularJS + ReactJS + Redux + CSS",
+        ".NET Core Microservices, MVC, WebAPI, xUnit.net",
+        "Swagger, Postman"
+    ];
+
+  export const testimonials =[
+    {
+        name: "D. Dusty Edenfield",
+        role: "Sr. Systems Architect",
+        image: "",
+        company: "Georgia Institute of Technology",
+        image: "./project-1.gif",
+        quote: "Ayoub Salhi worked at Georgia Southern University for one year from November 2014 to December 2015. Ayoub was a Student Web and Mobile Developer with responsibilities including but not limited to developing iOS and Android mobile apps as well as web applications. In this position, he reported to me and worked with two to three other student developers at any given time. I was the Lead Architect for Enterprise Web Development at Georgia Southern during Ayoub’s employment. Ayoub was an intelligent and skillful employee that I thoroughly enjoyed supervising. He was very se lf motivated and a quick learner who accomplished all the tasks that were assigned to him. If I gave him a project that involved technology that was new to him, he always had a great attitude and was able to use his research skills and solid work ethic to quickly learn new skills and deliver the project on time. From my perspective, it was obvious that other student employees liked Ayoub as well. He was always helpful to other employees that needed his assistance and he was also a good listener if he needed to receive help from others. If I ever have the opportunity to work with Ayoub again, I would not hesitate. He is a very bright individual and I’m sure the future holds great things for him. If you would like additional information about Ayoub, you can reach me at dusty.edenfield@oit.gatech.edu or call 404-385-4729. Sincerely",
+      },
+      {
+        name: "Rebekah L.Hickey",
+        role: "Assistant Director, Entreprise Technology Solutions",
+        image: "",
+        company: "Georgia Southern University",
+        image: "./project-1.gif",
+        quote: "I’m happy to recommend Ayoub Salhi as a talented, motivated web/software developer. He was employed as a student developer in the Enterprise Technology Solutions (formerly Enterprise Application Services) department of Information Technology Services at Georgia Southern University from October 2014 through December 2015. \n I supervised Ayoub in his role as student developer for a short period from October — December 2015 and in that time was impressed with his self-motivation, technical skills and enthusiasm for learning new technologies. Ayoub is responsible, polite and has a great attitude; he follows through on what he says he’ll do and meets deadlines. \n In his role with our department, Ayoub developed new web and mobile applications and provided troubleshooting and other assistance on existing sites and applications, demonstrating his knowledge of HTML5/CSS, PHP, MySQL, scripting and API programming. \n One of Ayoub’s strongest contributions to our department was in the development of mobile beacon technology. Ayoub presented his work on the mobile beacon project, including a functional demonstration of the technology, to key leadership at the University which enabled us to get the buy-in we needed to move forward with using the new technology on campus. \n If you would like to discuss anything further, please feel free to contact me at rhickey @georgiasouthern.edu. \n Sincerely,",
+      }
+  ] 
