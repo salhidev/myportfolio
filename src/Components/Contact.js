a9974d62e94884b8a12a3e7ad52287932cb6c546
Ayoub Salhi <salhi.dev@gmail.com>
2022-07-05 15:41:46 -0400

    map

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -36,7 +36,7 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=Bahamas&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            src="https://www.google.com/maps/embed/v1/place?q=Chesterfield&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
